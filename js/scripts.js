ff91be0075871830a69a46d3974f1e2812e049e5
Zach <azbastian@gmail.com>
2017-03-28 21:57:11 -0700

    it will add beginning user logic/business logic.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var leapYear = function(year) {
+return false;
+};
+
+// jQuery user interface logic here.
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
