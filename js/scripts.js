8d51267799b6a63344e8dc022521577f7f7b154a
Zach <azbastian@gmail.com>
2017-03-28 22:15:04 -0700

    it will add finishing back-end logic, final user interface logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,30 @@
+// business logic
+
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
-  } else {
+  } else  {
     return false;
   }
 };
 
-// jQuery user interface logic here.
+// user interface logic
 
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+    // $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {  // same: (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
