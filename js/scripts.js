7ef881a844c54455d8992bbc2afa36c106984cf1
Zach <azbastian@gmail.com>
2017-03-28 22:02:15 -0700

    it will add additional back-end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var leapYear = function(year) {
-  if (year % 4 === 0) {
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
     return true;
   } else {
     return false;
